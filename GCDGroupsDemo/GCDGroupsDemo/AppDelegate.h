31558ca5a3b8d4b6548281b59927588dbf61fddd
Maciej Sienkiewicz <maciej@macoscope.com>
2014-02-27 18:44:49 +0100

    Initial commit

---
+++ GCDGroupsDemo/GCDGroupsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GCDGroupsDemo
+//
+//  Created by Maciej Sienkiewicz on 27.02.2014.
+//  Copyright (c) 2014 Maciej Sienkiewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GCDGroupsDemo/GCDGroupsDemo/AppDelegate.m
@@ -0,0 +1,112 @@
+//
+//  AppDelegate.m
+//  GCDGroupsDemo
+//
+//  Created by Maciej Sienkiewicz on 27.02.2014.
+//  Copyright (c) 2014 Maciej Sienkiewicz. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#define SLEEP_TIME 1000
+#define TASKS_COUNT 10000
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+//  [self dispatchGroupAsyncMethod];
+  
+//  [self serialQueueAndCustomCounterMethod];
+  
+  [self dispatchGroupEnterLeaveMethod];
+
+  return YES;
+}
+
+
+// Method to simulate background task that we need to perform multiple times. It can be some delegate method with callback
+// or AFNetworking request operation with success and failure blocks or just NSURLConnection completion handler. Most likely
+// something beyond our control.
+- (void)taskWithNumber:(NSUInteger)number andCompletionBlock:(void (^)(void))completionBlock
+{
+  NSLog(@"Task number %lu", (unsigned long)number);
+  usleep(arc4random_uniform(SLEEP_TIME));
+  
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    usleep(arc4random_uniform(SLEEP_TIME));
+    completionBlock();
+  });
+}
+
+
+// Insufficient approach using dispatch_group_async from Apple Concurrency Programming Guide (Waiting on Groups of Queued Tasks)
+- (void)dispatchGroupAsyncMethod
+{
+  dispatch_group_t group = dispatch_group_create();
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  __weak AppDelegate *weakSelf = self;
+  
+  for (NSUInteger i = 0; i < TASKS_COUNT; ++i) {
+    dispatch_group_async(group, queue, ^{
+      [weakSelf taskWithNumber:i andCompletionBlock:^{
+        NSLog(@"End of task %lu", (unsigned long)i);
+      }];
+    });
+  }
+  
+  dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+  NSLog(@"All tasks done");
+}
+
+
+// Working but not clean solution using custom counter which needs to be synchronized (or serial queue must be used)
+- (void)serialQueueAndCustomCounterMethod
+{
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  __block NSUInteger inProgress = TASKS_COUNT;
+  __weak AppDelegate *weakSelf = self;
+  
+  for (NSUInteger i = 0; i < TASKS_COUNT; ++i) {
+    dispatch_async(queue, ^{
+      [weakSelf taskWithNumber:i andCompletionBlock:^{
+        NSLog(@"End of task %lu", (unsigned long)i);
+        
+        @synchronized(weakSelf) {
+          --inProgress;
+        }
+        
+        if (inProgress == 0) {
+          NSLog(@"All tasks done");
+        }
+      }];
+    });
+  }
+}
+
+
+// Nice and clean solution using dispatch_group_enter and dispatch_group_leave
+- (void)dispatchGroupEnterLeaveMethod
+{
+  dispatch_group_t group = dispatch_group_create();
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  __weak AppDelegate *weakSelf = self;
+  
+  for (NSUInteger i = 0; i < TASKS_COUNT; ++i) {
+    dispatch_async(queue, ^{
+      dispatch_group_enter(group);
+      [weakSelf taskWithNumber:i andCompletionBlock:^{
+        NSLog(@"End of task %lu", (unsigned long)i);
+        dispatch_group_leave(group);
+      }];
+    });
+  }
+  
+  dispatch_group_notify(group, queue, ^{
+    NSLog(@"All tasks done");
+  });
+  
+}
+
+
+@end
